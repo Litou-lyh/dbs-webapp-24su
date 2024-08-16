b6f3e6ebe2cd534f31c0a8259205a0ff2fc41c95
litou-lyh <1614895393@qq.com>
2024-08-16 21:38:04 +0000

    add update button logic

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -83,20 +83,24 @@ $(function () {
         }       
 
         if (error !== "") {
+            $("#flash_message").empty();
             flash_message("Error: Missing " + error.slice(1) + " fields!")
             return
         }
 
         if (!Number.isInteger(Number(age)) || Number(age) < 0) {
+            $("#flash_message").empty();
             flash_message("Error: Age should be positive integer!")
             return
         }
 
         if (!Number.isInteger(Number(children)) || Number(children) < 0) {
+            $("#flash_message").empty();
             flash_message("Error: Number of Children should be positive integer!")
             return
         }
         if (isNaN(Number(bmi)) || Number(bmi) < 0) {
+            $("#flash_message").empty();
             flash_message("Error: BMI should be positive number!")
             return
         }
@@ -143,16 +147,73 @@ $(function () {
         let id = $("#id").val();
         let first_name = $("#first_name").val();
         let last_name = $("#last_name").val();
-        let smoke = $("#smoke").val() == "true";
+        let smoke = $("#smoke").val();
+        let age = $("#age").val();
+        let bmi = $("#bmi").val();
+        let children = $("#children").val();
         let sex = $("#sex").val();
-        let birthday = $("#pet_birthday").val();
+        let region = $("#region").val();
+
+        let error = "";
+        if (!first_name || first_name === "") {
+            error += ", First Name"
+        }
+        if (!last_name || last_name === "") {
+            error += ", Last Name"
+        }
+        if (!age || age === "") {
+            error += ", Age"
+        }
+        if (sex === "Unknown") {
+            error += ", Sex"
+        }
+        if (!bmi || bmi === "") {
+            error += ", BMI"
+        }
+        if (!children || children === "") {
+            error += ", Children"
+        }
+        if (smoke === "Unknown") {
+            error += ", Smoke"
+        }
+        if (region === "Unknown") {     
+            error += ", Region"
+        }       
+
+        if (error !== "") {
+            $("#flash_message").empty();
+            flash_message("Error: Missing " + error.slice(1) + " fields!")
+            return
+        }
+
+        if (!Number.isInteger(Number(age)) || Number(age) < 0) {
+            $("#flash_message").empty();
+            flash_message("Error: Age should be positive integer!")
+            return
+        }
+
+        if (!Number.isInteger(Number(children)) || Number(children) < 0) {
+            $("#flash_message").empty();
+            flash_message("Error: Number of Children should be positive integer!")
+            return
+        }
+        if (isNaN(Number(bmi)) || Number(bmi) < 0) {
+            $("#flash_message").empty();
+            flash_message("Error: BMI should be positive number!")
+            return
+        }
+
+        smoke = smoke == "true";
 
         let data = {
             "first_name": first_name,
             "last_name": last_name,
-            "smoke": smoke,
+            "age": age,
+            "bmi": bmi,
             "sex": sex,
-            "birthday": birthday
+            "children": children,
+            "smoke": smoke,
+            "region": region,
         };
 
         $("#flash_message").empty();
